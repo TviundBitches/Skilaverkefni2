66a420dd42064bad56bd8f4983bc7df41478c138
sigrung15 <sigrung15@ru.is>
2017-02-13 22:30:08 +0000

    adding client and server

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import * as io from "socket.io-client";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  socket : any;
+
+  constructor() {
+    this.socket = io("http://localhost:8080/");
+    this.socket.on("connect", function(){
+        console.log("connect");
+    });
+  }
+}
