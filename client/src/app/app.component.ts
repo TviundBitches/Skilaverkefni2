c302e38afe9006ef650634ea1e30e87c3a86f244
sigrung15 <sigrung15@ru.is>
2017-02-15 15:37:54 +0000

    cattness

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'My Chat App';
+    title = 'KittyChats';
+    underTitle = 'Are you a cat, then chat';
 
     constructor() {}
 }
