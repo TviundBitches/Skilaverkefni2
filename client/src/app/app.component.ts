aca6a0e09a37cfa5acd6a0d0eb69a83eef448d90
Johanna Svovudottir <johannasvovudottir@gmail.com>
2017-02-19 16:23:19 +0000

    toastr

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
     title = 'KittyChats';
     underTitle = 'If you are a cat, then chat';
     logoutName = '';
 
-    constructor(private router: Router) {}
-    
+    constructor(private router: Router, private toastrService: ToastrService) {
+    }
+    showSuccess() {
+        this.toastrService.success('Hello world!', 'Toastr fun!');
+    }
     navToRooms(){
       this.router.navigate(['/rooms/']);
     }
