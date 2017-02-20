19dc11724c647c6e5bfde9528b462647ef275907
Johanna Svovudottir <johannasvovudottir@gmail.com>
2017-02-20 23:01:36 +0000

    sexy scrollbar

--- client/src/app/room/room.component.ts
+++ client/src/app/room/room.component.ts
@@ -99,13 +99,14 @@ export class RoomComponent implements OnInit {
     }
 
     onSendMsg() {
-        this.chatService.sendMsg(this.roomId, this.msg).subscribe(lst => {
+        this.chatService.sendMsg(this.roomId, this.userName + ' says: ' + this.msg).subscribe(lst => {
             if (this.roomId === lst[0]) {
                 this.msgs = [];
                 for (let i = 1; i < lst.length; i++) {
                     this.msgs.push(lst[i]);
                 }
             }
+
         });
         this.msg = '';
     }
